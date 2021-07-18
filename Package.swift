765f850747d76edb9f4bfdecb4404dbebdef4104
Ardalan Samimi <ardalan.samimi@gmail.com>
2021-07-18 22:48:51 +0200

    Add package

---
+++ Package.swift
@@ -0,0 +1,16 @@
+// swift-tools-version:5.3
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "Network",
+    products: [
+        .library(name: "Network", targets: ["Network"])
+    ],
+    dependencies: [],
+    targets: [
+        .target(name: "Network", dependencies: []),
+        .testTarget(name: "NetworkTests", dependencies: ["Network"])
+    ]
+)
