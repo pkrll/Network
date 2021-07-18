a80c808cb38e4d3f6d3a3746ddc42ae949697f54
Ardalan Samimi <ardalan.samimi@gmail.com>
2021-07-18 23:02:28 +0200

    Add HttpResult

---
+++ Sources/Network/Result/HttpResult.swift
@@ -0,0 +1,34 @@
+import Foundation
+
+public typealias HttpResult = Result<Response, HttpError>
+
+public extension HttpResult {
+    var request: Request {
+        switch self {
+        case .failure(let error):
+            return error.request
+        case .success(let response):
+            return response.request
+        }
+    }
+    
+    var response: Response? {
+        switch self {
+        case .failure(let error):
+            return error.response
+        case .success(let response):
+            return response
+        }
+    }
+    
+    init(request: Request, data: Data?, response: URLResponse?, error: Error?) {
+        do {
+            let response = try Response(for: request, with: response, data: data, error: error)
+            self = .success(response)
+        } catch let error as HttpError {
+            self = .failure(error)
+        } catch {
+            self = .failure(.init(code: .invalidResponse, request: request, underlyingError: error))
+        }
+    }
+}
