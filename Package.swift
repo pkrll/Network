cf664a03199ce89855a6d2ec29d7c26b830dacfa
Ardalan Samimi <ardalan.samimi@gmail.com>
2021-07-18 23:24:18 +0200

    Update Package

--- Package.swift
+++ Package.swift
@@ -5,9 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "Network",
-    products: [
-        .library(name: "Network", targets: ["Network"])
-    ],
+    products: [.library(name: "Network", targets: ["Network"])],
     dependencies: [],
     targets: [
         .target(name: "Network", dependencies: ["Shared"]),
