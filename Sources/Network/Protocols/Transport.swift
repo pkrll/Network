9d56e598d27543ad2acd8333b43f1e90d966891f
Ardalan Samimi <ardalan.samimi@gmail.com>
2021-07-23 22:32:27 +0200

    Add conditional import for FoundationNetworking

--- Sources/Network/Protocols/Transport.swift
+++ Sources/Network/Protocols/Transport.swift
@@ -1,4 +1,7 @@
 import Foundation
+#if canImport(FoundationNetworking)
+import FoundationNetworking
+#endif
 
 public protocol Transport {
     func send(_ request: URLRequest, _ completion: @escaping (Data?, URLResponse?, Error?) -> Void) -> TransportTask
